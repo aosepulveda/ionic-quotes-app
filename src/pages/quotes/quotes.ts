1a40a06744bc314f6a04c49d47eaca0e712ce184
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-01-27 16:41:11 -0300

    added alert

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @IonicPage()
@@ -10,7 +10,9 @@ import { Quote } from '../../data/quote.interface';
 export class QuotesPage implements OnInit {
   quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(private navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navCtrl: NavController, 
+              private navParams: NavParams,
+              private alertCtrl: AlertController) {
   }
 
   ngOnInit() {
@@ -20,5 +22,30 @@ export class QuotesPage implements OnInit {
   /*ionViewDidLoad() {
     this.quoteGroup = this.navParams.data;
   }*/
+
+  onAddToFavorite(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('OK');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled!!');
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
 
 }
