047d5c8de5272984e750237214ed4d70f9ab425f
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2018-01-27 12:33:54 -0300

    added quote card style

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Quote} from "../../data/quote.interface";
 import quotes from '../../data/quotes';
+import { QuotesPage } from '../quotes/quotes';
 
 @IonicPage()
 @Component({
@@ -10,7 +11,8 @@ import quotes from '../../data/quotes';
 })
 export class LibraryPage implements OnInit {
   quoteCollection: {category: string, quotes: Quote[], icon: string}[];
-
+  quotesPage = QuotesPage;
+  
   ngOnInit() {
     this.quoteCollection = quotes;
   }
